cca1a52d94064fbc57cbced9218b1a553df78b27
jacks524 <jacquyngonga@gmail.com>
2026-05-01 16:34:35 +0100

    m

--- core_access_and_identity_service_back/freeradius/docker-entrypoint.sh
+++ core_access_and_identity_service_back/freeradius/docker-entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 export PGSSLMODE=disable
 
 echo "Waiting for PostgreSQL at $DB_HOST:$DB_PORT..."
+echo "DB_USER configured: ${DB_USER:+yes}"
+echo "DB_NAME configured: ${DB_NAME:+yes}"
+echo "DB_PASS configured: ${DB_PASS:+yes}"
 until nc -z "$DB_HOST" "$DB_PORT" 2>/dev/null; do
   sleep 2
 done
